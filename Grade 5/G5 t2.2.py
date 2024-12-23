b778508c7bc777ba7f5682b6e86c0666b1743dcd
Avani <avani.abhijit@orchids.edu.in>
2024-12-23 12:26:33 +0530

    Add files via upload

--- Grade 5/G5 t2.2.py
+++ Grade 5/G5 t2.2.py
@@ -1,8 +1,9 @@
-#Modify below code and use the for loop to draw dash lines
 from turtle import Turtle
 t = Turtle()
-#Write for loop code below line.
-
+t.forward(10)
+t.penup()
+t.forward(10)
+t.pendown()
 t.forward(10)
 t.penup()
 t.forward(10)
