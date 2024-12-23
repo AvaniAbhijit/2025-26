632fa93f3e4f86af872a95cd2c06de132c84f9dc
Avani <avani.abhijit@orchids.edu.in>
2024-12-23 12:27:55 +0530

    Add files via upload

---
+++ Grade 5/Challenge 2.py
@@ -0,0 +1,11 @@
+#Modify the below code to draw the multiple dash lines using for loop.
+from turtle import Turtle
+t = Turtle()
+t.forward(10)
+t.penup()
+t.forward(10)
+t.pendown()
+t.forward(10)
+t.penup()
+t.forward(10)
+t.pendown()
