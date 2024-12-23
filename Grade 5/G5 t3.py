51f418ecf0dee83fec94f953e2f37fa96f075e1e
Avani <avani.abhijit@orchids.edu.in>
2024-12-23 13:02:21 +0530

    Add files via upload

---
+++ Grade 5/G5 t3.py
@@ -0,0 +1,10 @@
+
+from turtle import Turtle
+t= Turtle()
+def draw(num_sides):
+    for i in range(num_sides):
+        angle = 360/num_sides
+        t.forward(100)
+        t.right(angle)
+draw(5)
+
