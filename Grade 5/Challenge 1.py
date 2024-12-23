2c1db12c553935a1faf2bcd607c70daeb264221e
Avani <avani.abhijit@orchids.edu.in>
2024-12-23 12:08:36 +0530

    Add files via upload

---
+++ Grade 5/Challenge 1.py
@@ -0,0 +1,12 @@
+#Write code to draw the square using forward and left(90) command.
+from turtle import Turtle
+t = Turtle()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
