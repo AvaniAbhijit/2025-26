92a4508310b736dc09b533f9061f0cbd4f05d112
Avani <avani.abhijit@orchids.edu.in>
2024-12-23 11:49:29 +0530

    Add files via upload

---
+++ Grade 5/G5 t1.6.py
@@ -0,0 +1,8 @@
+#Read the turlte documentation and move turtle forward using forward command.
+#Use the right angle to turn the turtle
+from turtle import Turtle
+t = Turtle()
+t.shape("turtle")
+t.color("Red")
+t.forward(100)
+t.right(90)
