9d16b004a9e7578b8db3aec963185b7c4f893bc2
Avani <avani.abhijit@orchids.edu.in>
2025-03-01 11:58:04 +0530

    Update G5 Lab Activity 1.py

--- Grade 5/G5 Lab Activity 1.py
+++ Grade 5/G5 Lab Activity 1.py
@@ -3,3 +3,16 @@
 #✅ Be a new turtle object with the name of food.
 #✅ Have a circle shape
 #✅ Be red in colour
+
+
+import turtle
+t = turtle.Screen()
+t.title("Snake Game")
+t.bgcolor("Dark Green")
+t.setup(width=600, height=600)
+# Create a turtle object named 'head' that will represent the snake's head.
+head = turtle.Turtle()
+# Set the shape of the 'head' turtle to a square.
+head.shape("square")
+# Set the color of the 'head' turtle to black.
+head.color("black")
