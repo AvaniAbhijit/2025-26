77428e107d725b9daafec5ca50d53d200cb27665
Avani <avani.abhijit@orchids.edu.in>
2025-03-01 09:44:28 +0530

    Update G5 Home Activity  Challenge 1.py

--- Grade 5/G5 Home Activity  Challenge 1.py
+++ Grade 5/G5 Home Activity  Challenge 1.py
@@ -2,17 +2,19 @@
 #We will create a new game - “Paddle-Ball Game”, similar to the above game.
 #Let’s go ahead and create the objects required for the game:
 
-#Screen must have
-#Black background
-#Title as “Paddle and Ball Game”
-#Width and height as 600
+#Screen must have : 
+#                    Black background
+#                    Title as “Paddle and Ball Game”
+#                    Width and height as 600
+
 #Create a paddle object:
-#The shape is a square
-#colour is white
-#Position it at X = 0, Y = -250
-#Use shapesize(1,5) to stretch the length-wise to make it appear as a rectangle.
+#                     The shape is a square
+#                     colour is white
+#                     Position it at X = 0, Y = -250
+#                     Use shapesize(1,5) to stretch the length-wise to make it appear as a rectangle.
+
 #Create a ball object:
-#The shape is a circle
-#Colour is red
-#Position it at X = 0, Y = 100)
+#                     The shape is a circle
+#                     The colour is red
+#                     Position it at X = 0, Y = 100)
 
