b0c8237c0fa9f9a8b237124eb6c403e62340816f
Avani <avani.abhijit@orchids.edu.in>
2025-03-01 09:45:51 +0530

    Update G5 Home Activity Challenge 2.py

--- Grade 5/G5 Home Activity Challenge 2.py
+++ Grade 5/G5 Home Activity Challenge 2.py
@@ -1,1 +1,4 @@
+# CHALLENGE 2:
+# We will continue with our new game - “Paddle-Ball Game”. 
+# Write code to move the paddle only in left and right directions when the left and right arrow keys are pressed.
 
