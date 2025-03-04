6669ffafc77fb9fd2cf93ff7c0f8f393dbd2894a
Avani <avani.abhijit@orchids.edu.in>
2025-03-04 11:12:59 +0530

    Add files via upload

---
+++ Grade 5/G5 1.1 Start with python.py
@@ -0,0 +1,16 @@
+# What is Python?
+# Python is a computer language that helps us talk to the computer
+# and give it instructions. We can use Python to make games, solve math problems,
+# and even create animations and patterns with the help of python module.
+
+# What is Turtle in Python?
+# Turtle is a module in Python that lets us makes game, draw pictures and
+# shapes with pattern on the screen.
+
+# What is module?
+# A module in Python is like a toolbox that has ready-made tools (codes) to help us do different tasks.
+# We use a module by writing import command.
+
+import turtle
+
+
