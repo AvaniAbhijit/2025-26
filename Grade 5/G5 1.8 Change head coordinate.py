0502176fb7d535ff36bc213a7d5c272a26cd60f4
Avani <avani.abhijit@orchids.edu.in>
2025-03-05 13:22:18 +0530

    Update and rename G5 1.4 Change head coordinate.py to G5 1.8 Change head coordinate.py

---
+++ Grade 5/G5 1.8 Change head coordinate.py
@@ -0,0 +1,26 @@
+# turtle.goto(0,100) moves the turtle up to Y = 50
+# Line 21 moves the head turtle 100 pixels up along the Y coordinate 
+
+# Task: Change the head position to -50 for the x-axis and 200 for the y-axis.
+
+
+import turtle
+
+# turtle screen functions
+t = turtle.Screen()
+t.title("Snake Game")
+t.bgcolor("Dark Green")
+t.setup(width=600, height=600)
+
+# Snake Head
+head = turtle.Turtle()
+head.shape("square")
+head.color("black")
+
+#move the head turtle 100 pixels up along the Y coordinate
+head.goto(0,100)
+
+# Snake food
+food = turtle.Turtle()
+food.shape("circle")
+food.color("red")
