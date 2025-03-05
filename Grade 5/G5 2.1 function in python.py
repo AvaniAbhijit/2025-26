40037dd9bb92070647df0e82ea3822f38f3ee0d9
Avani <avani.abhijit@orchids.edu.in>
2025-03-05 15:14:03 +0530

    Add files via upload

---
+++ Grade 5/G5 2.1 function in python.py
@@ -0,0 +1,10 @@
+# add is a function here,
+# def is a keyword which we use to define the function.
+# a,b is a value
+
+
+def add(a, b):  # Function with inputs
+    print(a + b)
+
+add(4, 6)  # Calls function with 4 and 6
+add(10, 20)  # Calls function with 10 and 20
