bc87a2e7e00d6d7114fe8909555b418cfedb3d5b
Avani <avani.abhijit@orchids.edu.in>
2025-03-05 11:44:51 +0530

    Update and rename G5 1.3 Create a variable for snake head.py to G5 1.7 Create a variable for snake head.py

--- Grade 5/G5 1.7 Create a variable for snake head.py
+++ Grade 5/G5 1.7 Create a variable for snake head.py
@@ -1,11 +1,16 @@
+# head is a variable name which is storing turtle.Turtle() object.
+
+# Task: Set the color for the head on line 16.
+
 import turtle
 t = turtle.Screen()
 t.title("Snake Game")
 t.bgcolor("Dark Green")
 t.setup(width=600, height=600)
+
 # Create a turtle object named 'head' that will represent the snake's head.
 head = turtle.Turtle()
 # Set the shape of the 'head' turtle to a square.
 head.shape("square")
 # Set the color of the 'head' turtle to black.
-head.color("black")
+head.color("")
