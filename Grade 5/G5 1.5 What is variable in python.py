de6e1fb6644680164e32724d7b88f32684966878
Avani <avani.abhijit@orchids.edu.in>
2025-03-05 11:26:01 +0530

    Add files via upload

---
+++ Grade 5/G5 1.5 What is variable in python.py
@@ -0,0 +1,13 @@
+# Python variable use to store the value.
+# Age and Msg are the variable which is storing the value.
+
+# Task 1 - Uncomment the line 17 and 18 and run the code.
+# Task 2 - Correct the code line 17 and 18 and run the code.
+
+Age = 12
+Msg = "Happy Birthday"
+print(Msg)
+print( Age+1)
+
+#1 = ("Saesha")
+#full Name ="Saesha Saraf"
