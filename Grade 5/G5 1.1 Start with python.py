1e60c70bdc5b50e8d16fff1ddf63f36ecf98daa3
Avani <avani.abhijit@orchids.edu.in>
2025-03-05 15:50:27 +0530

    Update G5 1.1 Start with python.py

--- Grade 5/G5 1.1 Start with python.py
+++ Grade 5/G5 1.1 Start with python.py
@@ -1,3 +1,8 @@
+# Task 1: Run the code and check the output.
+# Task 2: Change the number for addition(line no.14) and multiplication (line no. 16).
+# Task 3: Write the print statement to subtract two numbers
+
+
 # Python helps us print a one-line statement.
 print("Hello, students!")
 
@@ -8,15 +13,4 @@ print("Let's learn to code!")
 # Python solves math problems.
 print(5 + 6)  # Addition example
 
-# Task 1: Multiply two numbers (e.g., 7 and 3) and display the result using print().
 print(7*3)  # Example solution (students should try with their numbers)
-
-# Task 2: Substract two numbers and display the result using print().
-
-
-
-
-
-
-
-
