108c181489d5b302ec10631295aaa5627086f590
Avani <avani.abhijit@orchids.edu.in>
2025-03-06 14:12:35 +0530

    Add files via upload

---
+++ Grade 8/Volume 1/Pong AI/G8 1.5.3 Nested if statements in Python.py
@@ -0,0 +1,12 @@
+# An if statement inside another if statement.
+
+# Task1 : Change the value of age to 20 and check the output.
+# Task 2: Write the code for else statement for both the if statemnet.
+# Note : For task 2 take care of indentation.
+
+age = 25
+if age >= 18:
+    print("You can open a bank account.")
+    if age >= 21:
+        print("You are also eligible for a credit card.")
+
