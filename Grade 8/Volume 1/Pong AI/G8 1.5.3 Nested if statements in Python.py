f21262604917f3f10438688448b34475d85a3787
Avani <avani.abhijit@orchids.edu.in>
2025-03-06 14:13:13 +0530

    Update G8 1.5.3 Nested if statements in Python.py

--- Grade 8/Volume 1/Pong AI/G8 1.5.3 Nested if statements in Python.py
+++ Grade 8/Volume 1/Pong AI/G8 1.5.3 Nested if statements in Python.py
@@ -1,8 +1,8 @@
 # An if statement inside another if statement.
 
-# Task1 : Change the value of age to 20 and check the output.
-# Task 2: Write the code for else statement for both the if statemnet.
-# Note : For task 2 take care of indentation.
+# Task1: Change the value of age to 20 and check the output.
+# Task 2: Write the code for the else statement for both the if statements.
+# Note: For task 2, take care of indentation.
 
 age = 25
 if age >= 18:
