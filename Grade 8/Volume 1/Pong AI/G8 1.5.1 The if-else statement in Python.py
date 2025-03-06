f89d0ba7c5486e4619d378405093b487231c2cc7
Avani <avani.abhijit@orchids.edu.in>
2025-03-06 13:35:35 +0530

    Add files via upload

---
+++ Grade 8/Volume 1/Pong AI/G8 1.5.1 The if-else statement in Python.py
@@ -0,0 +1,8 @@
+# The if statement runs code only if the condition is True means when the age is greater than 18.
+# The else block runs when the condition is False means when the age is less than 18.
+
+age = 16
+if age >= 18:
+    print("You can vote!")
+else:
+    print("You cannot vote.")
