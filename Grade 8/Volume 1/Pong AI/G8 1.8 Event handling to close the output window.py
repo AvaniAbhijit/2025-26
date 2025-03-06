793ae8116a8d104a1eefb77160831c82684567cf
Avani <avani.abhijit@orchids.edu.in>
2025-03-06 16:15:57 +0530

    Update G8 1.8 Event handling to close the output window.py

--- Grade 8/Volume 1/Pong AI/G8 1.8 Event handling to close the output window.py
+++ Grade 8/Volume 1/Pong AI/G8 1.8 Event handling to close the output window.py
@@ -1,7 +1,7 @@
 # The main loop in a Pygame program keeps the game running.
 # It repeats again and again until the player decides to quit.
 # We need event handling to listen to the player clicks.
-# if player clicks "X" then game quits as on line 20 and 21.
+# If the player clicks "X" then the game quits as on lines 20 and 22.
 
 import pygame
 pygame.init()
@@ -10,16 +10,14 @@ screen_width = 600
 screen_height = 500
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Pong')
+pygame.display.set_caption('Pong AI Game')
 
 # Main loop
-while True:
-
-    # Check for User Quit
-    for event in pygame.event.get():
+running = True
+while running:
+    for event in pygame.event.get():     # Check for User Quit
         if event.type == pygame.QUIT:
-            pygame.quit() # Quit Pygame
-            exit()        # Exit the program
-    # Update display
-    pygame.display.update()
+            running = False    
+    pygame.display.update()           # Update display
+pygame.quit()                         # Quit Pygame
 
