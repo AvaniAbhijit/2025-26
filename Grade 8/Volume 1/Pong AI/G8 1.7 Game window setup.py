34d89745ea6455ea1217660e0bb0d538904bcc69
Avani <avani.abhijit@orchids.edu.in>
2025-03-06 14:49:28 +0530

    Add files via upload

---
+++ Grade 8/Volume 1/Pong AI/G8 1.7 Game window setup.py
@@ -0,0 +1,18 @@
+# In this session, we will: Import necessary libraries , Initialize Pygame
+
+# Task 1:Define screen_width, screen_height variable on line 14,15 respectively and assign the value.
+
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+# Set up game window
+                            # width of the game window
+                            # height of the game window
+
+# Set up the game window
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Set the title of the game window to 'Pong'
+pygame.display.set_caption('Pong')
