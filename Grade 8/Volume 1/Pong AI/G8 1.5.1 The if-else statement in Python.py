b3e30ecaa75145a60e623d1771f1eb2a7724cb52
Avani <avani.abhijit@orchids.edu.in>
2025-03-06 13:54:20 +0530

    Update G8 1.5.1 The if-else statement in Python.py

--- Grade 8/Volume 1/Pong AI/G8 1.5.1 The if-else statement in Python.py
+++ Grade 8/Volume 1/Pong AI/G8 1.5.1 The if-else statement in Python.py
@@ -1,5 +1,10 @@
 # The if statement runs code only if the condition is True means when the age is greater than 18.
-# The else block runs when the condition is False means when the age is less than 18.
+# The else block runs when the condition is False, which means when the age is less than 18.
+# Observe the indentation(spacing) of the code within the if and else block on lines no.11 & 13.
+# Relational operators(==, !=, >, <, >=, and <=.)are used in the condition, observe on line no 9.
+
+# Task: Write the if statement to check num = 7 is an odd number or even number.
+# Hint: Use num % 2 == 0 as the if condition.
 
 age = 16
 if age >= 18:
