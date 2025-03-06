a8a626044dd9b91469298af9776386a58bfb11af
Avani <avani.abhijit@orchids.edu.in>
2025-03-06 12:10:50 +0530

    Update G8 1.4 Module and methods in python.py

--- Grade 8/Volume 1/Pong AI/G8 1.4 Module and methods in python.py
+++ Grade 8/Volume 1/Pong AI/G8 1.4 Module and methods in python.py
@@ -1,12 +1,12 @@
 # A module in Python (math & random) is a file that contains Python code,
-#   such as variables (pi), and methods (sqrt,factorial & randit).
+# such as variables (pi) and methods (sqrt, factorial & randit).
 
 # Module helps to avoid writing the same code again and again.
-# Use the import keyword to bring a module into our program as on line 6.
+# Use the import keyword to bring a module into our program as on line 11 and 17.
 # A method is a function inside a module that performs a specific task.
 
-#Task 1: Uncomment the line 14 & 15 , Run the code and check the output.
-#Task 2 : Change the values for math methods and random methods.
+#Task 1: Uncomment lines 17 & 18. Run the code and check the output.
+#Task 2: Change the values for math methods and random methods.
 
 import math  # Importing the math module
 print(math.sqrt(16))  # Using the sqrt() method to find the square root
