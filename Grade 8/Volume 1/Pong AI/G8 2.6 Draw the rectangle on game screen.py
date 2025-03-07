436705bfc10697d5edecc1cb29bace98b397b760
Avani <avani.abhijit@orchids.edu.in>
2025-03-07 10:49:18 +0530

    Update G8 2.6 Draw the rectangle on game screen.py

--- Grade 8/Volume 1/Pong AI/G8 2.6 Draw the rectangle on game screen.py
+++ Grade 8/Volume 1/Pong AI/G8 2.6 Draw the rectangle on game screen.py
@@ -1,11 +1,17 @@
+# On line no 39 pygame.draw.rect() is used to draw a rectangle shape as a line.
+# This method takes 3 parameters:
+#   - where to draw: the screen object
+#   - Color of the shape: WHITE
+#   - rectangular area : tuple : (x_coordinate, y_coordinate, width, height)
+# The x and y coordinates are the values of the top left position of the rectangle
+# All these variables are defined from line no26 to 29.
 
-
+# Task: Draw a GREEN rectangle with the same dimensions as the WHITE rectangle and
+#       touching the WHITE rectangle on the vertical side.
 
 import pygame
-import sys
 
 pygame.init()
-
 
 screen_width = 600
 screen_height = 500
@@ -14,7 +20,13 @@ pygame.display.set_caption('Pong AI Game')
 
 BG_COLOR = (50, 25, 50)
 WHITE=(255,255,255)
+GREEN=(0,255,0)
 
+# Rectangle position and dimension
+rect_x = 200    # x coordinate where the rectangle starts
+rect_y = 150    # y coordinate where the rectangle starts
+rect_width = 100    # width of the rectangle
+rect_height = 50    # height of the rectangle
 
 running = True
 while running:
@@ -24,16 +36,11 @@ while running:
 
     screen.fill(BG_COLOR)
 
-    # Draw a blue rectangle
-    rect_x = 200
-    rect_y = 150
-    rect_width = 100
-    rect_height = 50
+    # Draws the white rectangle on screen at rect_x, rect_y
     pygame.draw.rect(screen, WHITE, (rect_x, rect_y, rect_width, rect_height))
 
     # Update the display
-    pygame.display.flip()
+    pygame.display.update()
 
 # Quit Pygame
 pygame.quit()
-sys.exit()
