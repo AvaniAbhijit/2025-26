2cabce1dd8844b0181564eae95b61baf5a140ece
Avani <avani.abhijit@orchids.edu.in>
2025-03-15 16:29:31 +0530

    Update G5 4.1 Lists in Python.py

--- Grade 5/G5 4.1 Lists in Python.py
+++ Grade 5/G5 4.1 Lists in Python.py
@@ -3,6 +3,7 @@
 # We can make a list by using square brackets [ ] and putting items inside, separated by commas.
 # The .append() function is used to add an item to the end of a list.
 # The .clear() function is used to remove all items from the list, making it empty.
+# On line 25, New_fruits is an empty list created using [].
 # Each item in a list has a position number called an index. Python starts counting from 0 (not 1!).
 
 # Task1: Create a new list of your favorite colors and print it.
@@ -21,7 +22,12 @@ print("After appending Mango:",fruits)
 fruits.clear() # The .clear() function is used to remove all items from the list, making it empty.
 print("After clearing list:",fruits)
 
-fruits = ["apple", "banana", "cherry"]
-print("First fruit _index 0 in the list:",fruits[0])  # First item
-print("Second fruit _index 1 in the list:",fruits[1])  # Second item
-print("Third fruit _index 2 in the list:",fruits[2])  # Third item
+New_fruits =[]          # empty list
+print("Empty list:",New_fruits)
+New_fruits.append("apple")
+New_fruits.append("banana")
+New_fruits.append("cherry")
+print("List after append the fruits:",New_fruits)
+print("First fruit _index 0 in the list:",New_fruits[0])  # First item
+print("Second fruit _index 1 in the list:",New_fruits[1])  # Second item
+print("Third fruit _index 2 in the list:",New_fruits[2])  # Third item
