00899f3231ad813199987be9514a00dc492edab8
Avani <avani.abhijit@orchids.edu.in>
2025-03-15 14:55:14 +0530

    Add files via upload

---
+++ Grade 5/G5 4.1 Lists in Python.py
@@ -0,0 +1,27 @@
+# A list in Python is like a box that can hold many things together.
+# You can keep numbers, words, or a mix of both inside a list.
+# We can make a list by using square brackets [ ] and putting items inside, separated by commas.
+# The .append() function is used to add an item to the end of a list.
+# The .clear() function is used to remove all items from the list, making it empty.
+# Each item in a list has a position number called an index.Python starts counting from 0 (not 1!).
+
+# Task1: Create a new list of your favorite colors and print it.
+# Task2: Add a new color to the list using .append() and print the updated list.
+# Task3: Remove all items from the list using .clear() and print the empty list.
+# Task4: Create a list of 5 numbers and print the third number using indexing.
+
+
+# Here, fruits is a list that contains three items: apple, banana, and cherry
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+#fruits.append("Mango") # The .append() function is used to add an item to the end of a list.
+#print(fruits)
+
+#fruits.clear() # The .clear() function is used to remove all items from the list, making it empty.
+#print(fruits)
+
+#fruits = ["apple", "banana", "cherry"]
+#print(fruits[0])  # First item
+#print(fruits[1])  # Second item
+#print(fruits[2])  # Third item
