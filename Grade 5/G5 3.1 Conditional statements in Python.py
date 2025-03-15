222d3d71dc81fd6941caeeeab6da8209fcfc9839
Avani <avani.abhijit@orchids.edu.in>
2025-03-15 10:54:07 +0530

    Add files via upload

---
+++ Grade 5/G5 3.1 Conditional statements in Python.py
@@ -0,0 +1,21 @@
+# The if statement is used to make decisions.
+# Observe the indentation(spacing) of the code within the if block on lines no.11 & 13.
+# Relational operators(==, !=, >, <, >=, and <=.)are used in the condition, observe on line no 9.
+
+# Task 1: Change "red" to "green" or "yellow". Run and see the output
+# Task 2: Write a if statement on line 20 to check if number > 5.
+#         If so, then print "This is a big number!"
+
+light = "red"
+
+if light == "red":
+    print("Stop!")
+if light == "green":
+    print("Go!")
+if light == "yellow":
+    print("Slow down!")
+
+number = 10
+
+
+
