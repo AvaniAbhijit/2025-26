9469768dccc1a583c76b9b491124aacdc89b0411
Avani <avani.abhijit@orchids.edu.in>
2025-03-15 10:57:23 +0530

    Update G5 3.1 Conditional statements in Python.py

--- Grade 5/G5 3.1 Conditional statements in Python.py
+++ Grade 5/G5 3.1 Conditional statements in Python.py
@@ -1,9 +1,9 @@
 # The if statement is used to make decisions.
-# Observe the indentation(spacing) of the code within the if block on lines no.11 & 13.
-# Relational operators(==, !=, >, <, >=, and <=.)are used in the condition, observe on line no 9.
+# Observe the indentation(spacing) of the code within the if block on lines no.12,14 & 16.
+# Relational operators(==, !=, >, <, >=, and <=.) are used in the condition, observe on line no 11, 13 & 15.
 
-# Task 1: Change "red" to "green" or "yellow". Run and see the output
-# Task 2: Write a if statement on line 20 to check if number > 5.
+# Task 1: Change the value of light variable from "red" to "green" or "yellow" on line 9. Run and see the output
+# Task 2: Write an if statement on line 19 to check if number > 5.
 #         If so, then print "This is a big number!"
 
 light = "red"
