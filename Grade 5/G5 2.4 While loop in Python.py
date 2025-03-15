2e9386922f0703675aea0465939f9e7b04cb5929
Avani <avani.abhijit@orchids.edu.in>
2025-03-15 10:39:33 +0530

    Update G5 2.4 While loop in Python.py

--- Grade 5/G5 2.4 While loop in Python.py
+++ Grade 5/G5 2.4 While loop in Python.py
@@ -1,16 +1,16 @@
-# A loop is used when you need to repeat the same action.
-# While loop executes as long as the condition is not met.
-# Note the indentation(spacing) within while loop on the lines 18 and 19.
-# In the below example, the loop keeps running as long as chocolates are more than 0.
+# A loop in Python is used when you need to repeat the same action.
+# The while loop executes as long as the condition is not met.
+# Note the indentation(spacing) within while loop on lines 18 and 19.
+# In the example below, the loop keeps running as long as the number of chocolates is more than 0.
 
-# Task 1: Move the line 'chocolates = chocolates - 1' back -> in line with while.
-#        Run the code and observe the output. Why is it happening so?
+# Task 1: Press the backspace key to remove the space before the line on 19. 
+#        Run the code and observe the output. Why is this happening?
 
 # Task 2: Write a while loop to print numbers from 1 to 5 on line 24.
-#         Hint: use the condition number < 6 for the while loop.
+#         Hint: Use the condition number < 6 for the while loop.
 
-# Task 3: Uncomment lines 27 and 28. What do you see ?
-#        This is an infinte loop as the condition is always True.
+# Task 3: Uncomment lines 27 and 28. What do you see?
+#        This is an infinite loop as the condition is always True.
 
 chocolates = 5  # You have 5 chocolates
 
