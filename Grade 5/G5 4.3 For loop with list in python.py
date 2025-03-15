111b1916102d8a9725b3f1e352ade7603db44bde
Avani <avani.abhijit@orchids.edu.in>
2025-03-15 17:16:13 +0530

    Update G5 4.3 For loop with list in python.py

--- Grade 5/G5 4.3 For loop with list in python.py
+++ Grade 5/G5 4.3 For loop with list in python.py
@@ -1,14 +1,14 @@
 # A for loop helps us repeat something again and again without writing the same code multiple times.
 # The range() function helps us repeat something a fixed number of times in Python.
-# range(1,5,1) on line 7 means "start at 1 and stop before 5 and increase by 1.
-# The len() on line 13 function helps us count how many items are in a list.
-# range(5,1,-1) on line means "start at 5 and stop before 1 and decrease by 1.
+# range(1,5,1) on line 14 means "start at 1 and stop before 5 and increase by 1.
+# The len() on line 23 helps us count how many items are in a list.
+# range(5,1,-1) on line 18 means "start at 5 and stop before 1 and decrease by 1.
 
 
-# Task 1 : Run the code and observe the output.
-# Task 2 : Change the for loop range to print number till 5 for line 12.
-# Task 3 : Change the for loop range to print number till 0 for line 17.
-# Task 4 : add 55 to the numbers list and run the code and check the total.
+# Task 1: Run the code and observe the output.
+# Task 2: Change the for loop range to print numbers till 5 for line 14.
+# Task 3: Change the for loop range to print numbers till 0 for line 87.
+# Task 4: Add 55 to the numbers list on line 22, run the code, and check the total.
 
 print("Numbers 1 to 5")
 for i in range(1,5,1):
