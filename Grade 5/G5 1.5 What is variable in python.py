51069042a2da353e6155e0e4aebed01ead7e57ca
Avani <avani.abhijit@orchids.edu.in>
2025-03-25 10:53:40 +0530

    Update G5 1.5 What is variable in python.py

--- Grade 5/G5 1.5 What is variable in python.py
+++ Grade 5/G5 1.5 What is variable in python.py
@@ -1,8 +1,8 @@
 # Python variable used to store the value.
-# Age and Msg are the variables in the below code that store the value.
+# Age and Msg are the variables in the below code that store the value on lines 11 & 12.
 # The variable must not have a space in between.
 # The variable must not start with a number.
-# The variable must not have special characters except _.
+# The variable must not have special characters except "_".
 
 
 # Task 1 - Uncomment lines 16 and 17 and run the code.
