aafd48ea82e8729ec971afc011e07643f7d4f942
Avani <avani.abhijit@orchids.edu.in>
2025-03-26 16:42:37 +0530

    Update G8 1.6.2 while loop in python.py

--- Grade 8/Volume 1/Pong AI/G8 1.6.2 while loop in python.py
+++ Grade 8/Volume 1/Pong AI/G8 1.6.2 while loop in python.py
@@ -1,7 +1,8 @@
-# The while loop is used when you want to repeat instructions until a condition is false
+# The while loop is used when you want to repeat instructions until a condition is false.
 # True and False are Boolean values.
 
-# Task 1: Print the numbers from 20 to 1 using a while loop.
+# Task 1: Run the code and observe the output.
+# Task 2: Change the code to print the numbers in reverse order from 20 to 1 using a while loop.
 
 running = True
 num = 0
