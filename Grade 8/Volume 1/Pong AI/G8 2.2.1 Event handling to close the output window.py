76038d078f5af7400f192b26e2304d63380da7a4
Avani <avani.abhijit@orchids.edu.in>
2025-03-26 16:55:08 +0530

    Update G8 2.2.1 Event handling to close the output window.py

--- Grade 8/Volume 1/Pong AI/G8 2.2.1 Event handling to close the output window.py
+++ Grade 8/Volume 1/Pong AI/G8 2.2.1 Event handling to close the output window.py
@@ -1,7 +1,7 @@
 # The main loop in a Pygame program keeps the game running.
 # It repeats again and again until the player decides to quit.
 # We need event handling to listen to the player clicks.
-# pygame.event.get() on line 22 listens to - Keyboard input (KEYDOWN, KEYUP),  Mouse clicks and Window closing (QUIT event)
+# pygame.event.get() on line 22 listens to - Keyboard input (KEYDOWN, KEYUP),  Mouse clicks and Window closing (QUIT event).
 # If the player clicks "X, " the game quits as on lines 23 and 24.
 
 # Task 1: Write code for another if statement on line 25 to check for event.type == pygame.KEYDOWN: 
