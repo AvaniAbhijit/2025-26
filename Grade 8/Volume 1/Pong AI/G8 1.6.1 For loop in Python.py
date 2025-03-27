6a5e24be1945d2ba4c29505059d0ef8922ceb623
Avani <avani.abhijit@orchids.edu.in>
2025-03-27 09:38:16 +0530

    Update G8 1.6.1 For loop in Python.py

--- Grade 8/Volume 1/Pong AI/G8 1.6.1 For loop in Python.py
+++ Grade 8/Volume 1/Pong AI/G8 1.6.1 For loop in Python.py
@@ -1,17 +1,19 @@
 # Loops help to repeat instructions multiple times until a specific condition is met.
-# for loop used when you know how many times to repeat the instructions.
+# The for loop is used when you know how many times to repeat the instructions.
 # Observe the indentation of code within the for loop.
 
-# Task 1: Write a for loop to print numbers from 120 to 145.
-# Task 2: Write a for loop to print the letters of your name.
+# Task 1: Run the code and observe the output. 
+# Task 2: Uncomment lines no. 15 & 16, run the code, and observe the output.
+# Task 3: Change the range in the for loop on line 11 to print numbers from 120 to 145.
 
-# repeats 10 times from 1 to 10. Last number in range not included.
+
+# repeats 10 times from 1 to 10. The last number in a range is not included.
 for num in range(1, 11):
-    print(num, " in for loop")
+    print(num)
 
-# repeats based on number of letters in the string - Orchids
-for letter in "Orchids":
-    print(letter)
+# repeats based on the number of letters in the string - Orchids
+#for letter in "Orchids":
+#    print(letter)
 
 
 
