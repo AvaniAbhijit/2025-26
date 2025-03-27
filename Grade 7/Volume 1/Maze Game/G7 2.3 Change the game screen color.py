d05f3a556913aef3bc683a9d42f7b8c4f1684ff5
Avani <avani.abhijit@orchids.edu.in>
2025-03-27 10:36:40 +0530

    Add files via upload

--- Grade 7/Volume 1/Maze Game/G7 2.3 Change the game screen color.py
+++ Grade 7/Volume 1/Maze Game/G7 2.3 Change the game screen color.py
@@ -1,21 +1,20 @@
-
 # screen.fill on line 27 is used to give the screen color.
 # BG_COLOR on line 17 is a tuple variable contaning RGB values.
 # A tuple is a Python datatype that is an ordered list of elements within round brackets ().
 
-# Task 1: Create a WHITE tuple variable on line 18 with value (255, 255, 255).
-# Task 2 :Change the screen color to WHITE.
+# Task 1: Create a RED tuple variable on line 18 with value (255,0,0).
+# Task 2 :Change the screen color to RED.
 
 import pygame
 pygame.init()
 
-screen_width = 600
+screen_width = 500
 screen_height = 500
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Maze Game')
 
 # Define colors
-BG_COLOR = (50, 25, 50)  # Dark purple background
+BG_COLOR = (255, 255, 255)  # White background
 
 
 running = True
