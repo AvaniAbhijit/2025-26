bb2add81c262fe58057e3adbb7d8fd784739b3b5
Avani <avani.abhijit@orchids.edu.in>
2025-03-27 16:07:05 +0530

    Update G7 1.6.1 For loop in Python.py

--- Grade 7/Volume 1/Maze Game/G7 1.6.1 For loop in Python.py
+++ Grade 7/Volume 1/Maze Game/G7 1.6.1 For loop in Python.py
@@ -2,8 +2,17 @@
 # The for loop is used when you know how many times to repeat the instructions.
 # Observe the indentation of code within the for loop.
 
-# Task: Write a for loop to print the letters of your name.
+# Task 1: Run the code and observe the output.
+# Task 2: Uncomment lines 14 to 18, run the code, and observe the output.
+# Task 3: Write a for loop to print the letters of your name from line 19 onward.
+
 
 # repeats based on number of letters in the string - Orchids
 for letter in "Orchids":
     print(letter)
+
+#for letter in "School":
+#    if letter in "aeiouAEIOU":  # Check if the letter is a vowel
+#        print(letter, "- Vowel")
+#    else:
+#        print(letter, "- Consonant")
