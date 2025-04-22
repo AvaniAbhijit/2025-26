78412e58cf0b5b903d0d44d9e3f6522799e6338e
Avani <avani.abhijit@orchids.edu.in>
2025-04-22 15:10:05 +0530

    Add files via upload

---
+++ Grade 5/Volume 2/G5 6.1 Fruit Catching Game Setup.py
@@ -0,0 +1,16 @@
+#Task 1: Import the turtle module.
+
+#Task 2: Create a screen object.
+#Use turtle.Screen() to create the main game window.
+
+#Task3:Set the title of the game.
+#Give your game a title like "Fruit Catcher Game" using .title().
+
+#Task4: Change the background color.
+#Use a color of your choice using .bgcolor() (e.g., "lightblue").
+
+#Task5: Set the window size to 800x600 pixels.
+#Use .setup(width=800, height=600).
+
+#Task6: Turn off automatic screen updates.
+#Use .tracer(0) — this gives you more control over screen refreshes.
