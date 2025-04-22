ff9b190bb06f51034290298a1632fd10465b3946
Avani <avani.abhijit@orchids.edu.in>
2025-04-22 15:29:18 +0530

    Add files via upload

---
+++ Grade 5/Volume 2/G5 6.2 Create the Basket.py
@@ -0,0 +1,13 @@
+#Task1: Create a turtle object named basket.
+#Use basket = turtle.Turtle() to create a new Turtle.
+
+#Task2: Change the shape of the turtle to a square.
+
+#Task3: Change the color of the basket.
+
+#Task4: Resize the turtle to make it look like a basket.
+#Use basket.shapesize(stretch_wid=1, stretch_len=5) to stretch the square into a rectangle.
+
+#Task5: Lift the pen so it doesn’t draw when moving.
+
+#Task6: Move the basket to the bottom of the screen to (0, -250).
