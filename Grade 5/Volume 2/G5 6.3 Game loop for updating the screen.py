6f10e57e534456f317be4e675936c25c16aff71e
Avani <avani.abhijit@orchids.edu.in>
2025-04-25 15:18:53 +0530

    Add files via upload

---
+++ Grade 5/Volume 2/G5 6.3 Game loop for updating the screen.py
@@ -0,0 +1,22 @@
+#Task 1: Run the code and observe the output.
+#Task 2: Change the size and position of the basket.
+#Task 3: Change game_on to False on line no 19.
+
+import turtle
+screen = turtle.Screen()
+screen.title("Fruit Catcher Game")
+screen.bgcolor("lightblue")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+basket = turtle.Turtle()
+basket.shape("square")
+basket.color("brown")
+basket.shapesize(1,5)
+basket.penup()
+basket.goto(0, -250)
+
+game_on = True
+while game_on:
+    screen.update()
+screen.mainloop()
