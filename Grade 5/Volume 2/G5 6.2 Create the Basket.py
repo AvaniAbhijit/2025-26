9ce082ee4cd7d2e593088d81af6b33c97054ba5f
Avani <avani.abhijit@orchids.edu.in>
2025-04-25 11:25:55 +0530

    Update G5 6.2 Create the Basket.py

--- Grade 5/Volume 2/G5 6.2 Create the Basket.py
+++ Grade 5/Volume 2/G5 6.2 Create the Basket.py
@@ -1,4 +1,4 @@
-# Task 1: Create a turtle object named basket which can be used to catch the falling fruits.
+# Task 1: Create a turtle object named basket to catch falling fruits.
 
 # Task 2: Change the shape of the turtle to a square.
 
@@ -10,3 +10,10 @@
 # Task 5: Lift the pen so it doesn’t draw when moving.
 
 # Task 6: Move the basket to the bottom of the screen to (0, -250).
+
+import turtle
+screen = turtle.Screen()
+screen.title("Fruit Catcher Game")
+screen.bgcolor("lightblue")
+screen.setup(width=800, height=600)
+screen.tracer(0)
