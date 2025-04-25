ecda52c69f7cf2ee4610d3b4efcf83f4d53bcb41
Avani <avani.abhijit@orchids.edu.in>
2025-04-25 11:08:50 +0530

    Add files via upload

---
+++ Grade 5/Volume 2/G5 6.3 Revision of function & conditonal statement in Python.py
@@ -0,0 +1,16 @@
+#Task  : Inside the function write the conditional statement for following-
+#If num is greater than 0, print "The number is positive."
+#If num is less than 0, print "The number is negative."
+#If num is equal to 0, print "The number is zero."
+
+
+def check_number(num):
+
+                        # If the number is greater than 0
+
+                        # If the number is less than 0
+
+                        # If the number is exactly 0
+
+# Call the function with the user input
+check_number(6)
