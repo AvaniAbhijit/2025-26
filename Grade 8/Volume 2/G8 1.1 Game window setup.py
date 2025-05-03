5282b01f7bad43fb4d36ab50b4f997e923147909
Avani <avani.abhijit@orchids.edu.in>
2025-05-03 11:31:46 +0530

    Update G8 1.1 Game window setup.py

--- Grade 8/Volume 2/G8 1.1 Game window setup.py
+++ Grade 8/Volume 2/G8 1.1 Game window setup.py
@@ -1,11 +1,14 @@
 # Basic Setup & Initialization
 
-# Task 1: Import pygame module and initialize the module
-# Task 2: Create variables - SCREEN_HEIGHT and SCREEN_WIDTH with 600 and 400 as values.
-# Task 3: Define variables for WHITE, BLACK and SKY_BLUE (153, 217, 234) colors.
-# Task 4: Create the pygame screen variable using set_mode()
-# Task 5: Set the title of the pygame window to 'Toggle Jump'
-# Task 6: Write the event handler loop to handle the QUIT event within the while loop.
+# Task 1: Import the pygame module and initialize the module on lines 11 & 12.
+# Task 2: Create variables - SCREEN_HEIGHT and SCREEN_WIDTH with 600 and 400 as values on line 14 & 15.
+# Task 3: Define variables for WHITE, BLACK, and SKY_BLUE (153, 217, 234) colors on lines 17,18 & 19, respectively.
+# Task 4: Create the pygame screen variable using set_mode() on line 21.
+# Task 5: Set the title of the pygame window to 'Toggle Jump' on line 23.
+# Task 6: Write the event handler loop to handle the QUIT event within the while loop on lines 34 to 37.
+
+# Import the pygame module 
+
 
 # screen variables
 
@@ -14,9 +17,9 @@
 
 
 
-# create pygame screen
+# create a pygame screen
 
-#set the title of the screen
+# Set the title of the screen
 
 #set frame rate
 clock = pygame.time.Clock()
