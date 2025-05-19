1fc296019d22ef2855c2bb7330b8db9aa446df02
Avani <avani.abhijit@orchids.edu.in>
2025-05-19 16:37:36 +0530

    Update G8 7.4 Create Platform with inheritance and Group.py

--- Grade 8/Volume 2/G8 7.4 Create Platform with inheritance and Group.py
+++ Grade 8/Volume 2/G8 7.4 Create Platform with inheritance and Group.py
@@ -1,17 +1,16 @@
-# Add a platform for the player to jump on. Create a Platform class as given in task.
-# Image for platform is declared as platform_image on line
+# Add a platform for the player to jump on. Create a Platform class as given in the task.
+# Image for the platform is declared as platform_image on line
 # pygame.sprite.Group() on line __ creates a container to hold multiple platform objects
-# Line 70 to 75: Create a platform group, platform object and add the platform to the group
+# Line 70 to 75: Create a platform group, platform object, and add the platform to the group
 
 # Task 1: Create a Platform class that inherits from pygame.sprite.Sprite
-# Task 2: Define the init method which takes x, y, width and moving as parameters.
+# Task 2: Define the init method which takes x, y, and width as parameters.
 # Task 3: Call the init method of the Base class - pygame.sprite.Sprite.__init__(self)
 # Task 4: Define the following attributes of the class within this method:
 #       image: platform_image and scale it to (width, 10)
-#       moving: moving
 #       rect: Use get_rect() method on the image.
 #       Set the rect.x as the x value received as parameter
-#       Set the rect.y as the y value received as parameter
+#       Set the rect.y as the y value received as a parameter
 # Task 5: Call draw on the platform_group object.
 
 
@@ -46,18 +45,19 @@ bg_image = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 platform_image = pygame.image.load('land.png').convert_alpha()
 
 
-class Player():
-	def __init__(self, x, y):
-		self.image = pygame.transform.scale(player, (45, 45))
-		self.width = 25
-		self.height = 40
-		self.rect = pygame.Rect(0, 0, self.width, self.height)
-		self.rect.center = (x, y)
-		self.vel_y = 0
-		self.flip = False
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)  # Initialize Sprite class
+        self.image = pygame.transform.scale(player_image, (45, 45))  # Set image
+        self.width = 25
+        self.height = 40
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = (x, y)
+        self.vel_y = 0
+        self.flip = False
 
-	def draw(self):
-		screen.blit(pygame.transform.flip(self.image,self.flip, False), (self.rect.x - 12, self.rect.y - 5))
+    def draw(self):
+        screen.blit(self.image, (self.rect.x - 12, self.rect.y - 5))
 
 #platform class
 
@@ -70,7 +70,7 @@ player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
 # Create an empty Sprite Group
 platform_group = pygame.sprite.Group()
 # Create starting platform at a certain position and width.
-platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 50, 100, False)
+platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 50, 100)
 # Add the platform to the group
 platform_group.add(platform)
 
