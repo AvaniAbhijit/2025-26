4b5296175f1fbb7af2defb96aa45db422a37865b
Avani <avani.abhijit@orchids.edu.in>
2025-05-20 16:39:06 +0530

    Update G8 7.1 Inheritance in Python.py

--- Grade 8/Volume 2/G8 7.1 Inheritance in Python.py
+++ Grade 8/Volume 2/G8 7.1 Inheritance in Python.py
@@ -1,8 +1,14 @@
-# Base Class: Animal
-# Derived Class: Dog
-# Derived class inherits from Animal using the syntax - class Dog(Animal).
-# Animal class has a method sound() that prints: "Animal makes a sound" using the animal object
-# Dog class overrides the sound() method to print: "Buddy says Woof!" using the Dog object
+# The code demonstrates inheritance in Python using classes.
+# A base class Animal is defined, and a derived class Dog inherits from it using the syntax class Dog(Animal).
+# An animal has: An __init__ method (constructor) to store the name. A method sound() that prints a generic message: "Animal makes a sound".
+# Dog inherits the __init__ method from Animal (so it doesn't need its own).
+# It overrides the sound() method to give a custom behavior: prints "Buddy says Woof!" (or any other name passed).
+# An Animal object a is created, and it uses the base class method.
+# A Dog object d is created, and it uses the overridden sound() method.
+
+# Inheritance: Dog class inherits properties and methods from Animal.
+# Method Overriding: Dog class overrides the inherited method to customize behavior.
+# Object Creation and Method Calling: Shows how objects use the correct method based on their class.
 
 import pygame
 
