96fe2f6e8a9af92ba953997c887194acad5acefb
Avani <avani.abhijit@orchids.edu.in>
2025-05-31 11:31:36 +0530

    Update G5 6.2 Create the Basket.py

--- Grade 5/Volume 2/G5 6.2 Create the Basket.py
+++ Grade 5/Volume 2/G5 6.2 Create the Basket.py
@@ -1,15 +1,14 @@
+# basket = turtle.Turtle() creates the basket object that the player will control.
+# basket.shape("square") will change the shape of the turtle to a square.
+# basket.shapesize(stretch_wid=1, stretch_len=5) resize the turtle to make it look like a rectangular basket.
+
 # Task 1: Create a turtle object named basket to catch falling fruits.
-
 # Task 2: Change the shape of the turtle to a square.
-
 # Task 3: Change the color of the basket.
-
 # Task 4: Resize the turtle to make it look like a basket 
 #         using basket.shapesize(stretch_wid=1, stretch_len=5) to stretch the square into a rectangle.
-
 # Task 5: Lift the pen so it doesn’t draw when moving.
-
-# Task 6: Move the basket to the bottom of the screen to (0, -250).
+# Task 6: Move the basket to the bottom of the screen to (0, 250).
 
 import turtle
 screen = turtle.Screen()
