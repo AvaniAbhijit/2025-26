cac7742b54f31adcda555b83d52a547a82ac7b68
Avani <avani.abhijit@orchids.edu.in>
2025-05-31 11:46:47 +0530

    Update G5 6.4 Revision of function & conditional statement in Python.py

--- Grade 5/Volume 2/G5 6.4 Revision of function & conditional statement in Python.py
+++ Grade 5/Volume 2/G5 6.4 Revision of function & conditional statement in Python.py
@@ -1,4 +1,7 @@
-#Task : Inside the function, write the conditional statement for the following-
+# check_number function takes one input called num on line 10.
+# Inside this function, you need to write conditional statements (if-else checks) on line 12 onwards.
+
+# Task: Inside the function, write the conditional statement for the following-
 #If num is greater than 0, print "The number is positive" on lines 8 & 9.
 #If num is less than 0, print "The number is negative" on lines 10 & 11.
 #If num is equal to 0, print "The number is zero" on lines 12 & 13.
