fc6911c6fcee460e38a7e8df284a2faff495ecd1
Avani <avani.abhijit@orchids.edu.in>
2025-06-08 11:19:17 +0530

    Update G5 6.2 Create the Basket.py

--- Grade 5/Volume 2/G5 6.2 Create the Basket.py
+++ Grade 5/Volume 2/G5 6.2 Create the Basket.py
@@ -2,9 +2,9 @@
 # basket.shape("square") will change the turtle's shape to a square.
 # basket.shapesize(stretch_wid=1, stretch_len=5) resize the turtle to make it look like a rectangular basket.
 
-# Task 1: Change the shape of the turtle to a square.
-# Task 2: Change the color of the basket.    
-# Task 3: Lift the pen so it doesn’t draw when moving.
+# Task 1: Change the shape basket to a square.
+# Task 2: Change the basket color to brown.   
+# Task 3: Lift the basket(basket.penup()) to prevent drawing lines.
 
 
 import turtle
@@ -16,10 +16,10 @@ screen.tracer(0)
 
 # Create the basket turtle
 basket = turtle.Turtle()                  # Create a basket object.
-                                          # Task 1: Change shape to a square.
-                                          # Task 2: Change the basket color.
+                                          # Task 1: Change the shape basket to a square.
+                                          # Task 2: Change the basket color to brown.
 #using basket.shapesize(stretch_wid=1, stretch_len=5) to stretch the square into a rectangle.
 basket.shapesize(stretch_wid=1, stretch_len=5)  #  Resize to look like a rectangle
-                                          # Task 3: Lift the pen to prevent drawing lines.
+                                          # Task 3: Lift the basket(basket.penup()) to prevent drawing lines.
 basket.goto(0, -250)                      #  Move basket to bottom of screen.
 screen.update()                           # Manually refreshes the screen since automatic updates are off.
