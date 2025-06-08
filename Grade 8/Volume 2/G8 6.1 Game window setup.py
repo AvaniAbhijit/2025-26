9c855d8ba77605a3f47ab3e9c193b8042a326dee
Avani <avani.abhijit@orchids.edu.in>
2025-06-08 11:37:14 +0530

    Update G8 6.1 Game window setup.py

--- Grade 8/Volume 2/G8 6.1 Game window setup.py
+++ Grade 8/Volume 2/G8 6.1 Game window setup.py
@@ -4,7 +4,7 @@
 # Task 2: Create variables - SCREEN_HEIGHT and SCREEN_WIDTH with 600 and 400 as values on line 14 & 15.
 # Task 3: Define variables for WHITE, BLACK, and SKY_BLUE (153, 217, 234) colors on lines 17,18 & 19, respectively.
 # Task 4: Create the pygame screen variable using set_mode() on line 21.
-# Task 5: Set the title of the pygame window to 'Toggle Jump' on line 23.
+# Task 5: Set the title of the pygame window to 'Sky Jump' on line 23.
 # Task 6: Write the event handler loop to handle the QUIT event within the while loop on lines 34 to 37.
 
 # Import the pygame module 
