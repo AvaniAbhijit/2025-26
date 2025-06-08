285fab6bc536f370058af64cfa0de2e1e82fc5ea
Avani <avani.abhijit@orchids.edu.in>
2025-06-08 11:16:08 +0530

    Update G5 6.2 Create the Basket.py

--- Grade 5/Volume 2/G5 6.2 Create the Basket.py
+++ Grade 5/Volume 2/G5 6.2 Create the Basket.py
@@ -1,14 +1,11 @@
 # basket = turtle.Turtle() creates the basket object that the player will control.
-# basket.shape("square") will change the shape of the turtle to a square.
+# basket.shape("square") will change the turtle's shape to a square.
 # basket.shapesize(stretch_wid=1, stretch_len=5) resize the turtle to make it look like a rectangular basket.
 
-# Task 1: Create a turtle object named basket to catch falling fruits.
-# Task 2: Change the shape of the turtle to a square.
-# Task 3: Change the color of the basket.
-# Task 4: Resize the turtle to make it look like a basket 
-#         using basket.shapesize(stretch_wid=1, stretch_len=5) to stretch the square into a rectangle.
-# Task 5: Lift the pen so it doesn’t draw when moving.
-# Task 6: Move the basket to the bottom of the screen to (0, 250).
+# Task 1: Change the shape of the turtle to a square.
+# Task 2: Change the color of the basket.    
+# Task 3: Lift the pen so it doesn’t draw when moving.
+
 
 import turtle
 screen = turtle.Screen()
@@ -17,21 +14,12 @@ screen.bgcolor("lightblue")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-screen.update()
+# Create the basket turtle
+basket = turtle.Turtle()                  # Create a basket object.
+                                          # Task 1: Change shape to a square.
+                                          # Task 2: Change the basket color.
+#using basket.shapesize(stretch_wid=1, stretch_len=5) to stretch the square into a rectangle.
+basket.shapesize(stretch_wid=1, stretch_len=5)  #  Resize to look like a rectangle
+                                          # Task 3: Lift the pen to prevent drawing lines.
+basket.goto(0, -250)                      #  Move basket to bottom of screen.
+screen.update()                           # Manually refreshes the screen since automatic updates are off.
