7f3526a4f3ec117449f5479a392b62f2c313ed41
Avani <avani.abhijit@orchids.edu.in>
2025-08-19 17:52:20 +0530

    Update G9 & G10 2.4 (b) -  Mac Extract text from Image using pytesseract.py

--- Grade 9 & Grade 10/G9 & G10 2.4 (b) -  Mac Extract text from Image using pytesseract.py
+++ Grade 9 & Grade 10/G9 & G10 2.4 (b) -  Mac Extract text from Image using pytesseract.py
@@ -1,10 +1,10 @@
 # pytesseract library (a Python wrapper for Google’s Tesseract-OCR engine)
 # to extract text from an image file.
-
+# Download any png file with text and save it as image.png in the same folder as your Python code.
 
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
-#add the name of image file (ensure that image and file are saved in same directory)
+#add the name of the image file (ensure that the image and file are saved in the same directory)
 image_file="image.png"
 #extract text from the image using pytesseract
 extracted_text = pytesseract.image_to_string(image_file)
