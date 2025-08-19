7754141c7f508da80de567974b04af945082c1dd
Avani <avani.abhijit@orchids.edu.in>
2025-08-19 17:53:22 +0530

    Update G9 & G10 2.6 (b) - Mac Convert image to grayscale.py

--- Grade 9 & Grade 10/G9 & G10 2.6 (b) - Mac Convert image to grayscale.py
+++ Grade 9 & Grade 10/G9 & G10 2.6 (b) - Mac Convert image to grayscale.py
@@ -3,7 +3,7 @@
 
 import pytesseract
 import cv2
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
 image_file="image.png"
 image = cv2.imread(image_file)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #Converts color image to grayscale for simplicity.
