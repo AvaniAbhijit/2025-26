3019298f5e02ab4d53a3f504bbc33f1524d2f655
Avani <avani.abhijit@orchids.edu.in>
2025-08-19 17:53:41 +0530

    Update G9 & G10 2.6 (b) - Mac Convert image to grayscale.py

--- Grade 9 & Grade 10/G9 & G10 2.6 (b) - Mac Convert image to grayscale.py
+++ Grade 9 & Grade 10/G9 & G10 2.6 (b) - Mac Convert image to grayscale.py
@@ -1,5 +1,6 @@
-
-
+# cv2.cvtColor() is an OpenCV function used to convert images from one color space to another.
+# image is originally in BGR format (Blue-Green-Red), as read by cv2.imread().
+# cv2.COLOR_BGR2GRAY tells OpenCV to convert the image from BGR to grayscale.
 
 import pytesseract
 import cv2
