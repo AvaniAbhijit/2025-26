32b058b1e05d4c3c78934b8265e192af0f4dc87a
Avani <avani.abhijit@orchids.edu.in>
2025-08-19 18:04:27 +0530

    Update G9 & G10 2.9(b) - Mac: Use of position constants.py

--- Grade 9 & Grade 10/G9 & G10 2.9(b) - Mac: Use of position constants.py
+++ Grade 9 & Grade 10/G9 & G10 2.9(b) - Mac: Use of position constants.py
@@ -1,11 +1,12 @@
-# Line 31 function named extract_text_from_image,
-# passing in a variable image_file_path as its argument.
-# The function extract_text_from_image() is expected to use
-# Optical Character Recognition (OCR) to extract text from that image.
-# The resulting text is then stored in the variable extracted_text
+# On line 41 : text_widget: This is the name of a Text widget, which allows the user to display
+# or type multi-line text in a window.
+#.insert(): This is a method that inserts text into the Text widget.
+# tk.END :This tells Tkinter where to insert the text.
+# tk.END means: “insert at the end of the current content” of the text widget.
 
-# Task: Insert Extracted Text into a Text Widget using insert on line 38.
-#        Hint : text_widget.insert(extracted_text)
+# Task 1: Enable text widget for insertion on line 40.
+#          Hint : text_widget.config(state="normal")
+# Task 2 : Disable text widget state = "disabled" after insertion on line 42.
 
 
 from tkinter import filedialog
@@ -33,8 +34,11 @@ def mark_attendance():
     if not image_file_path:
         print("No image selected.")
         return
-    extracted_text = extract_text_from_image(image_file_path)#pass the image selected by user and fetch the extracted text
-    print(extracted_text)#printing the extracted text on console
+    extracted_text = extract_text_from_image(image_file_path)
+    print(extracted_text)
+
+
+    text_widget.insert(tk.END, extracted_text + "\n")
 
 
 
@@ -44,32 +48,37 @@ root.geometry("500x500")
 
 root.configure(bg='skyblue')
 
-label1 = tk.Label(root, text="Smart Attendance System", font=("Courier New", 24), bg="blue", fg="white")
+label1 = tk.Label(root, text="Smart Attendance System", font=("Courier New", 24),
+bg="blue", fg="white")
 label1.pack(padx=10,pady=10)
 
-date_label = tk.Label(root, text="Select Date:",font=("Courier New", 10), bg="skyblue")
+date_label = tk.Label(root, text="Select Date:",font=("Courier New", 10),
+bg="skyblue")
 date_label.pack(pady=5)
 
 date_var = tk.StringVar()
 
-date_entry = DateEntry(root,relief="sunken",borderwidth=3,justify="center",state="normal",selectbackground="blue",textvariable=date_var)
+date_entry = DateEntry(root,relief="sunken",borderwidth=3,
+justify="center",state="normal",selectbackground="blue",textvariable=date_var)
 date_entry.pack(pady=5)
 
 date_var.trace('w', update)
 
-button1 = tk.Button(root, text="Select Excel File",width=20,height=2,bg="#B4A3D8",activebackground="grey",activeforeground="white",command=select_file)
+button1 = tk.Button(root, text="Select Excel File",width=20,height=2,bg="#B4A3D8",
+activebackground="grey",activeforeground="white",command=select_file)
 button1.config(highlightbackground = "black",highlightthickness=3)
 button1.pack(pady=10)
 
-button2=tk.Button(root,text="Upload Photo",width=20,height=2,bg="#B4A3D8",activebackground="grey",activeforeground="white",command=mark_attendance)
+button2=tk.Button(root,text="Upload Photo",width=20,height=2,bg="#B4A3D8",
+activebackground="grey",activeforeground="white",command=mark_attendance)
 button2.config(highlightbackground = "black",highlightthickness=3)
 button2.pack(pady=10)
 
 text_widget = tk.Text(root,width=50,height=5,spacing1=5,spacing2=5,spacing3=5,state="disabled")
-
 text_widget.pack(pady=10)
 
-submit=tk.Button(root,text="Submit",width=20,height=2,bg="#B4A3D8",activebackground="blue",activeforeground="white")
+submit=tk.Button(root,text="Submit",width=20,height=2,bg="#B4A3D8",
+activebackground="blue",activeforeground="white")
 submit.config(highlightbackground = "black",highlightthickness=3)
 submit.pack(pady=10)
 
